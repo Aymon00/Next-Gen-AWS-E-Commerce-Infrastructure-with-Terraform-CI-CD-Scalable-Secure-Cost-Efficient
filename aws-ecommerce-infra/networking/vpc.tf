5becc543378820ec367b9c6b7fc7169b393ee4a0
Ayman mohamed <105709688+Aymon00@users.noreply.github.com>
2025-02-09 22:02:38 +0200

    vpc.tf

--- aws-ecommerce-infra/networking/vpc.tf
+++ aws-ecommerce-infra/networking/vpc.tf
@@ -1,1 +1,10 @@
+# 1- Define the VPC 
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
 
+  tags = {
+    Name        = var.vpc_name
+    Environment = "project_environment"
+    Terraform   = "true"
+  }
+}
