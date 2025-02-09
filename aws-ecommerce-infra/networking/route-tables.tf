15c981065dbdc21dbf86e8fb2bb61b2688176ac5
Ayman mohamed <105709688+Aymon00@users.noreply.github.com>
2025-02-09 22:22:22 +0200

    route-tables.tf

---
+++ aws-ecommerce-infra/networking/route-tables.tf
@@ -0,0 +1,41 @@
+# 7- Create route tables for private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "project_private_rtb"
+    Terraform = "true"
+  }
+}
+# 8- Create route table associatioin for Private subnets.
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+# 9- Create route table for public subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name      = "project_public_rtb"
+    Terraform = "true"
+  }
+}
+#10- Create route table associations for Public subnets
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
